c520bab5e2628d3ac0c16955381e81a363771b8e
grcanosa <grcanosa@gmail.com>
2017-04-24 15:53:55 +0200

    Added slider step info

--- apps/slider_step.py
+++ apps/slider_step.py
@@ -6,5 +6,8 @@ import appdaemon.appapi as appapi
 
 class TimedSlider(appapi.AppDaemon)
     def initialize(self):
-        self.slider_name = self.args["slider"];
+        self.slider_name = self.args["slider"]
+        self.step = self.get_state(entity = self.slider_name, attribute = "step")
+        self.max_val = self.get_state(entity = self.slider_name, attribute = "max")
+        self.min_val = self.get_state(entity = self.slider_name, attribute = "min")
         self.listen_state(self.state_change, self.args["switch"], new="on")
