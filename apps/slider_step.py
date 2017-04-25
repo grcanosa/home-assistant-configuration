07f9503980d475a5a64e66d381e515b3f180dba5
grcanosa <grcanosa@gmail.com>
2017-04-25 11:10:53 +0200

    Added modifications

--- apps/slider_step.py
+++ apps/slider_step.py
@@ -6,8 +6,13 @@ import appdaemon.appapi as appapi
 
 class TimedSlider(appapi.AppDaemon)
     def initialize(self):
-        self.slider_name = self.args["slider"]
+        self.slider = self.args["input_slider"]
+        self.sensor = self.args["sensor"]
+        self.active = self.args["input_boolean"]
         self.step = self.get_state(entity = self.slider_name, attribute = "step")
         self.max_val = self.get_state(entity = self.slider_name, attribute = "max")
         self.min_val = self.get_state(entity = self.slider_name, attribute = "min")
+        self.unit = self.get_state(entity = self.slider_name, attribute = "unit")
         self.listen_state(self.state_change, self.args["switch"], new="on")
+
+    def state_change(self,)
